8e6164cd796d93b3faa69b93b5d69778bd04b49e
infoofajit <infoofajit@gmail.com>
2023-03-26 15:44:16 +0530

    API KEY FIX

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = 'AIzaSyARWaHkrLdKWOKg27o2feMXOIAEgXYmIUw';
+const GOOGLE_API_KEY = '';
 
 export const YOUTUBE_VIDEOS_API = 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=30&regionCode=IN&key='+GOOGLE_API_KEY
 
